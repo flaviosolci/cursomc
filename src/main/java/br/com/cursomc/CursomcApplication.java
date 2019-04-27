2799b36ed9e2beb8e2a76082602d89835caadd5e
Flavio Solci <flaviosolci@gmail.com>
2019-04-27 19:28:26 -0300

    added lombok. Default categories (insert in h2 at startup)

--- src/main/java/br/com/cursomc/CursomcApplication.java
+++ src/main/java/br/com/cursomc/CursomcApplication.java
@@ -1,14 +1,31 @@
 package br.com.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(final String[] args) {
-
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) throws Exception {
+		final Categoria categoria = new Categoria("Informática");
+		final Categoria categoria2 = new Categoria("Escritório");
+		repository.saveAll(Arrays.asList(categoria, categoria2));
+
 	}
 
 }
--- src/main/java/br/com/cursomc/domain/Categoria.java
+++ src/main/java/br/com/cursomc/domain/Categoria.java
@@ -1,67 +1,25 @@
 package br.com.cursomc.domain;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Categoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@NonNull
 	private String nome;
-
-	public Categoria() {
-	}
-
-	public Categoria(final Integer id, final String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(final Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(final String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof Categoria)) {
-			return false;
-		}
-		final Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Categoria [id=%s, nome=%s]", id, nome);
-	}
 
 }
